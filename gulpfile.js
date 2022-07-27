a9d1fcfcd478b77d34cc0915ecb52b753b8414dc
Alexey Ocheretenkp <b8er2la@outlook.com>
2022-07-27 09:29:24 +0300

    dirrectory tree rework. pug added; if gulpfile pug added;

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 import gulp from "gulp";
+import pug from 'gulp-pug';
+
+gulp.task('pug-compile', function () {
+    return gulp.src('src/**/*.pug')
+        .pipe(
+            pug({})
+        )
+        .pipe(gulp.dest('dist'));
+});
+
+// watch
+gulp.watch('src/**/*.pug', gulp.parallel('pug-compile'));
+
+// default
+gulp.task('default', gulp.parallel('pug-compile'));
